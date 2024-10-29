01846a1486a4eb81debb10e3931fa00063180ce6
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-10-29 22:49:31 +0500

    Refactor server.js to remove rate limiting and helmet middleware, and update CORS configuration

--- server.js
+++ server.js
@@ -1,40 +1,16 @@
 const express = require("express");
-const rateLimit = require("express-rate-limit");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const helmet = require("helmet");
-
 dotenv.config();
 require("./src/backup.js");
 require("./src/connection.js");
 const routes = require("./src/routes/router.js");
 const PORT = 5000;
 
-const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 100,
-  message: "Too many requests from this IP, please try again after a minute",
-  handler: (req, res) => {
-    console.log(`Rate limit reached for IP: ${req.ip}`);
-    res
-      .status(429)
-      .send("Too many requests from this IP, please try again after a minute");
-  },
-});
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "https://leetcode-admin-kiut.vercel.app",
-      "https://teacher-leetcode.vercel.app",
-      "https://student-leetcode.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
-app.use(helmet());
-app.use("/api", limiter, routes);
+app.use(cors());
+app.use("/api", routes);
 app.get("/", (req, res) => {
   return res.json({ message: "Server is running!" });
 });
