d74176ef37b64ff54e980995c992cf2e740ab2a1
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-10-29 22:40:17 +0500

    Add PORT=5000 to .env file and update response message in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const rateLimit = require("express-rate-limit");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const helmet = require("helmet");
+
 dotenv.config();
 require("./src/backup.js");
 require("./src/connection.js");
@@ -13,8 +14,13 @@ const limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 100,
   message: "Too many requests from this IP, please try again after a minute",
+  handler: (req, res) => {
+    console.log(`Rate limit reached for IP: ${req.ip}`);
+    res
+      .status(429)
+      .send("Too many requests from this IP, please try again after a minute");
+  },
 });
-
 const app = express();
 app.use(express.json());
 app.use(
@@ -27,21 +33,25 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
-
 app.use(helmet());
 app.use("/api", limiter, routes);
 app.get("/", (req, res) => {
-  return res.json({ message: "Server is run!" });
+  return res.json({ message: "Server is running!" });
 });
 
 function startServerOnPort(port) {
   const listen = app.listen(port, () =>
-    console.log(`server is running ${port}`)
+    console.log(`Server is running on port ${port}`)
   );
-  listen.on("error", () => {
-    console.log(`Port ${port} is busy. Trying a different port...`);
-    startServerOnPort(port + 1);
+  listen.on("error", (error) => {
+    if (error.code === "EADDRINUSE") {
+      console.log(`Port ${port} is busy. Trying a different port...`);
+      startServerOnPort(port + 1);
+    } else {
+      console.error(`Server error: ${error.message}`);
+    }
   });
 }
-
 startServerOnPort(PORT);
+
+module.exports = app;
