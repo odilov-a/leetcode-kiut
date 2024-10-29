e7c43dd0aac052aeb3c88ab17bf35f9b3d41a132
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-10-29 10:14:52 +0500

    Refactor server.js to update response message

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/api", routes);
 app.get("/", (req, res) => {
-  return res.json({ message: "Server is run!!!" });
+  return res.json({ message: "Server is run!!" });
 });
 
 function startServerOnPort(port) {
