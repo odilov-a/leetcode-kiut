ba83202bd22e0496b99260ba76a52ebf98622b2f
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-10-29 22:21:22 +0500

    Refactor package.json to add express-rate-limit and helmet dependencies

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const helmet = require("helmet");
 dotenv.config();
 require("./src/backup.js");
 require("./src/connection.js");
 const routes = require("./src/routes/router.js");
 const PORT = 5000;
 
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again after a minute",
+});
+
 const app = express();
 app.use(express.json());
-app.use(cors());
-app.use("/api", routes);
+app.use(
+  cors({
+    origin: [
+      "https://leetcode-admin-kiut.vercel.app",
+      "https://teacher-leetcode.vercel.app",
+      "https://student-leetcode.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use(helmet());
+app.use("/api", limiter, routes);
 app.get("/", (req, res) => {
-  return res.json({ message: "Server is run!!!!!" });
+  return res.json({ message: "Server is run!" });
 });
 
 function startServerOnPort(port) {
