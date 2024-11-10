7e87953a2c949afea5458783fb0ec94e409095bf
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-11-10 22:35:19 +0500

    Add endpoint to retrieve a random project for review in Pereview routes

--- src/routes/pereview.routes.js
+++ src/routes/pereview.routes.js
@@ -4,6 +4,8 @@ const { authenticate } = require("../middlewares/auth.middleware.js");
 const { requireRole } = require("../middlewares/role.middleware.js");
 const pereviewRouter = Router();
 
+pereviewRouter.get("/random", authenticate, requireRole(["student"]), pereviewController.getRandomProjectForReview);
+
 pereviewRouter.post("/", authenticate, requireRole(["student"]), pereviewController.submitProjectToPereview);
 
 pereviewRouter.get("/project/:projectId", authenticate, pereviewController.getPereview);
@@ -13,7 +15,5 @@ pereviewRouter.get("/reviewer/:reviewerId", authenticate, requireRole(["teacher"
 pereviewRouter.get("/:id", authenticate, pereviewController.getPereviewById);
 
 pereviewRouter.put("/:id", authenticate, requireRole(["teacher", "admin", "student"]), pereviewController.updatePereview);
-
-pereviewRouter.get("/random", authenticate, requireRole(["student"]), pereviewController.getRandomProjectForReview);
 
 module.exports = pereviewRouter;
