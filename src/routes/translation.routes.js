6b3a62a6ee60364a8561a54ffbd2f3c24cd57bc7
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-11-19 22:56:27 +0500

    Update translation routes to allow access for student and teacher roles

--- src/routes/translation.routes.js
+++ src/routes/translation.routes.js
@@ -4,10 +4,10 @@ const { requireRole } = require("../middlewares/role.middleware.js");
 const { authenticate } = require("../middlewares/auth.middleware.js");
 const translationRouter = Router();
 
-translationRouter.get("/", authenticate, requireRole(["admin"]), translationController.getAll);
-translationRouter.get("/:lang", authenticate, requireRole(["admin"]), translationController.findByLang);
-translationRouter.get("/search/:message", authenticate, requireRole(["admin"]), translationController.search);
-translationRouter.post("/:lang", authenticate, requireRole(["admin"]), translationController.create);
-translationRouter.put("/:id", authenticate, requireRole(["admin"]), translationController.update);
+translationRouter.get("/", authenticate, requireRole(["admin", "student", "teacher"]), translationController.getAll);
+translationRouter.get("/:lang", authenticate, requireRole(["admin", "student", "teacher"]), translationController.findByLang);
+translationRouter.get("/search/:message", authenticate, requireRole(["admin", "student", "teacher"]), translationController.search);
+translationRouter.post("/:lang", authenticate, requireRole(["admin", "student", "teacher"]), translationController.create);
+translationRouter.put("/:id", authenticate, requireRole(["admin", "student", "teacher"]), translationController.update);
 
 module.exports = translationRouter;
