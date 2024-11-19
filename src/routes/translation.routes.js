4004eb53971a19924bb4611c627a2cce5c101396
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-11-19 22:58:30 +0500

    Remove authentication and role-based access control from translation routes

--- src/routes/translation.routes.js
+++ src/routes/translation.routes.js
@@ -1,13 +1,11 @@
 const { Router } = require("express");
 const translationController = require("../controllers/translation.controller.js");
-const { requireRole } = require("../middlewares/role.middleware.js");
-const { authenticate } = require("../middlewares/auth.middleware.js");
 const translationRouter = Router();
 
-translationRouter.get("/", authenticate, requireRole(["admin", "student", "teacher"]), translationController.getAll);
-translationRouter.get("/:lang", authenticate, requireRole(["admin", "student", "teacher"]), translationController.findByLang);
-translationRouter.get("/search/:message", authenticate, requireRole(["admin", "student", "teacher"]), translationController.search);
-translationRouter.post("/:lang", authenticate, requireRole(["admin", "student", "teacher"]), translationController.create);
-translationRouter.put("/:id", authenticate, requireRole(["admin", "student", "teacher"]), translationController.update);
+translationRouter.get("/", translationController.getAll);
+translationRouter.get("/:lang", translationController.findByLang);
+translationRouter.get("/search/:message", translationController.search);
+translationRouter.post("/:lang", translationController.create);
+translationRouter.put("/:id", translationController.update);
 
 module.exports = translationRouter;
